921aa2d6b80cad351a51d512a7cb8f0d50bfda11
jiexu10 <jie.xu.10@gmail.com>
2016-01-05 14:57:44 -0500

    fixed hound style issues

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,8 +1,7 @@
 class Review < ActiveRecord::Base
   validates :rating,
     presence: true,
-    numericality: { only_integer: true },
-    inclusion: { in: (1..5) }
+    numericality: { only_integer: true }, inclusion: { in: (1..5) }
 
   RATING_LIST = [1, 2, 3, 4, 5]
 end
