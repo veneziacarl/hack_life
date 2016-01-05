811cf7b1e4c2ef247f6a6bdfaceb8d7337853ca4
jiexu10 <jie.xu.10@gmail.com>
2016-01-05 15:19:47 -0500

    updated review model to use rating list as validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
+  RATING_LIST = [1, 2, 3, 4, 5]
+
   validates :rating,
     presence: true,
-    numericality: { only_integer: true }, inclusion: { in: (1..5) }
-
-  RATING_LIST = [1, 2, 3, 4, 5]
+    numericality: { only_integer: true }, inclusion: { in: RATING_LIST }
 end
