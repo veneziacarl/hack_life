ec74ab596ff9ea74c784e2c7bf2e43e1b7e86e6d
jiexu10 <jie.xu.10@gmail.com>
2016-01-05 11:15:18 -0500

    user sign up feature complete with unit and feature tests

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ActiveRecord::Base
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+end
---
+++ spec/features/users/01_user_signs_up_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+feature 'user signs up', %Q{
+  As an unauthenticated user
+  I want to sign up
+  So that I can participate in the website
+} do
+
+  # ACCEPTANCE CRITERIA
+  # [ ] I must specify a valid email address
+  # [ ] I must specify a password and confirm that password
+  # [ ] If I do not give valid information, I get an error message
+  # [ ] If I specify valid information, I register my account and am authenticated
+
+  scenario 'user specifies valid and required information' do
+    visit root_path
+    click_link 'Sign Up'
+
+    fill_in 'First Name', with: 'firstname'
+    fill_in 'Last Name', with: 'lastname'
+    fill_in 'Email', with: 'fake@email.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'password'
+    click_button 'Sign Up'
+
+    expect(page).to have_content('Welcome to the club!')
+    expect(page).to have_content('Sign Out')
+  end
+
+  scenario 'required information is not supplied' do
+    visit root_path
+    click_link 'Sign Up'
+
+    click_button 'Sign Up'
+    expect(page).to have_content("can't be blank")
+    expect(page).to_not have_content('Sign Out')
+  end
+
+  scenario 'password does not match confirmation' do
+    visit root_path
+    click_link 'Sign Up'
+
+    fill_in 'First Name', with: 'firstname'
+    fill_in 'Last Name', with: 'lastname'
+    fill_in 'Email', with: 'fake@email.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password Confirmation', with: 'anotherpassword'
+    click_button 'Sign Up'
+
+    expect(page).to have_content("doesn't match")
+    expect(page).to_not have_content('Sign Out')
+  end
+end
