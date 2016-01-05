086003970912d3bfbd88091514869a2caca72801
jiexu10 <jie.xu.10@gmail.com>
2016-01-05 14:21:01 -0500

    add review feature and tests complete

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,28 @@
+class ReviewsController < ApplicationController
+  def index
+    @reviews = Review.all
+  end
+
+  def new
+    # @lifehack
+    @review = Review.new
+    @rating_list = Review::RATING_LIST
+  end
+
+  def create
+    @review = Review.new(review_params)
+
+    if @review.save
+      redirect_to reviews_path, notice: 'Review made!'
+    else
+      @rating_list = Review::RATING_LIST
+      flash.now[:error] = "Review rating can't be blank!"
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:rating, :comment)
+  end
+end
---
+++ app/models/review.rb
@@ -0,0 +1,5 @@
+class Review < ActiveRecord::Base
+  validates :rating, presence: true, numericality: { only_integer: true }, inclusion: { in: (1..5), message: "Must be between 1 - 5"}
+
+  RATING_LIST = [1,2,3,4,5]
+end
