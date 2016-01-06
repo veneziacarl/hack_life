09d8d278d9cb8c33cbac77011adcfa669ab7df4f
KodeT <talia.koss@gmail.com>
2016-01-06 14:26:11 -0500

    [Resolve 28] fixing hounds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,15 +7,14 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 User.create([{
-  first_name: 'Jon',
-  last_name: 'Knot',
-  email: 'jon@gmail.com',
-  password: 'whattheheck?' }]
+    first_name: 'Jon',
+    last_name: 'Knot',
+    email: 'jon@gmail.com',
+    password: 'whattheheck?' }]
                )
 Lifehack.create([{
-  title: 'How to tie a tie',
-  description: 'tie a knot',
-  creator_id: "#{User.first.id}"  }, {
-  title: 'How to tie a tie1',
-  creator_id: "#{User.first.id}"  }]
-)
+    title: 'How to tie a tie',
+    description: 'tie a knot',
+    creator_id: "#{User.first.id}"  }, {
+    title: 'How to tie a tie1',
+    creator_id: "#{User.first.id}" }])
