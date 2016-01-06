13500fcd0fd169cb5ba1cd8374e40daa64a2c2a8
KodeT <talia.koss@gmail.com>
2016-01-06 12:54:43 -0500

    [Resolves 28] hound fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   has_many :lifehacks
-    validates :first_name, presence: true
-    validates :last_name, presence: true
-    # Include default devise modules. Others available are:
-    # :confirmable, :lockable, :timeoutable and :omniauthable
-    devise :database_authenticatable, :registerable,
-      :recoverable, :rememberable, :trackable, :validatable
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+    :recoverable, :rememberable, :trackable, :validatable
 end
