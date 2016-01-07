b73b80ab18fbad4b9eef69828c71f8f2240a9602
KodeT <talia.koss@gmail.com>
2016-01-07 14:41:11 -0500

    fixed hound pt3

--- app/models/lifehack.rb
+++ app/models/lifehack.rb
@@ -2,12 +2,11 @@ class Lifehack < ActiveRecord::Base
   belongs_to :creator, class_name: 'User'
   has_many :reviews
   validates :title, presence: true
-
   before_validation :strip_whitespace
-  
+
   private
 
   def strip_whitespace
-    title = title.strip unless title.nil?
+    title.strip unless title.nil?
   end
 end
