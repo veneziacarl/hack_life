f56b278841fc85c79f8481be6f0b63233c5b0b9d
jiexu10 <jie.xu.10@gmail.com>
2016-01-12 13:24:46 -0500

    finished adding default profile pic through default_url path in carrierwave

--- app/uploaders/profile_photo_uploader.rb
+++ app/uploaders/profile_photo_uploader.rb
@@ -10,4 +10,8 @@ class ProfilePhotoUploader < CarrierWave::Uploader::Base
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
+
+  def default_url(*args)
+    ActionController::Base.helpers.asset_path("default/" + [version_name, "default_profile.png"].compact.join('_'))
+  end
 end
