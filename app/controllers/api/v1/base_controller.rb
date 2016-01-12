15972fbb3cd637fd01b93285b6700b2c781a1b40
jiexu10 <jie.xu.10@gmail.com>
2016-01-12 10:27:08 -0500

    fix hound issues in voting.js

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::BaseController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :not_found
 
   def not_found
-    return api_error(status: 404, errors: 'Not found')
+    api_error(status: 404, errors: 'Not found')
   end
 
   def api_error(status: 500, errors: [])
