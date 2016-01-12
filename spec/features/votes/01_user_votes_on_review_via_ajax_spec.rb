68022f006ec9d152fb697f17997ee8b7b3e758ea
jiexu10 <jie.xu.10@gmail.com>
2016-01-12 16:41:47 -0500

    delete extra javascript tests - DRF

--- spec/features/votes/01_user_votes_on_review_via_ajax_spec.rb
+++ spec/features/votes/01_user_votes_on_review_via_ajax_spec.rb
@@ -22,187 +22,52 @@ feature "user adds vote to review via ajax", %{
   scenario 'user upvotes review', js: true do
     user_sign_in(user)
     visit lifehack_path(review.lifehack)
-    within('.review') do
-      expect(page).to have_content("Score: 0")
-    end
 
-    expect_no_page_reload do
-      click_button '+1'
-
-      within('.review') do
-        expect(page).to have_content("Score: 1")
-      end
-    end
-  end
-
-  scenario 'multiple users upvote the same review', js: true do
-    user_sign_in(user)
-    visit lifehack_path(review.lifehack)
     click_button '+1'
 
-    click_link 'Sign Out'
-
-    user_sign_in(user2)
-    visit lifehack_path(review.lifehack)
-
-    expect_no_page_reload do
-      click_button '+1'
-
-      within('.review') do
-        expect(page).to have_content("Score: 2")
-      end
-    end
+    expect(page).to have_content("Score: 1")
+    expect(page).to_not have_content("Vote cast!")
   end
 
   scenario 'user downvotes review', js: true do
     user_sign_in(user)
     visit lifehack_path(review.lifehack)
 
-    expect_no_page_reload do
-      click_button '-1'
+    click_button '-1'
 
-      within('.review') do
-        expect(page).to have_content("Score: -1")
-      end
-    end
+    expect(page).to have_content("Score: -1")
+    expect(page).to_not have_content("Vote cast!")
   end
 
   scenario 'user can only make 1 up vote on a single review', js: true do
+    FactoryGirl.create(:vote, user: user, review: review)
     user_sign_in(user)
     visit lifehack_path(review.lifehack)
 
-    expect_no_page_reload do
-      click_button '+1'
-      within('.review') do
-        expect(page).to have_content("Score: 1")
-      end
+    click_button '+1'
 
-      click_button '+1'
-
-      expect(page).to have_content('You have already upvoted this review!')
-      within('.review') do
-        expect(page).to have_content("Score: 1")
-      end
-    end
+    expect(page).to have_content('You have already upvoted this review!')
+    expect(page).to have_content("Score: 1")
   end
 
   scenario 'user can only make 1 down vote on a single review', js: true do
+    FactoryGirl.create(:vote, :down, user: user, review: review)
     user_sign_in(user)
     visit lifehack_path(review.lifehack)
 
-    expect_no_page_reload do
-      click_button '-1'
-      within('.review') do
-        expect(page).to have_content("Score: -1")
-      end
+    click_button '-1'
 
-      click_button '-1'
-
-      expect(page).to have_content('You have already downvoted this review!')
-      within('.review') do
-        expect(page).to have_content("Score: -1")
-      end
-    end
+    expect(page).to have_content('You have already downvoted this review!')
+    expect(page).to have_content("Score: -1")
   end
 
   scenario 'user can change vote on already voted review', js: true do
+    FactoryGirl.create(:vote, user: user, review: review)
     user_sign_in(user)
     visit lifehack_path(review.lifehack)
-    expect_no_page_reload do
-      click_button '+1'
 
-      within('.review') do
-        expect(page).to have_content("Score: 1")
-      end
-      click_button '-1'
+    click_button '-1'
 
-      within('.review') do
-        expect(page).to have_content("Score: -1")
-      end
-    end
-  end
-
-  scenario 'user can up vote for multiple reviews on the same lifehack', js: true do
-    user_sign_in(user)
-    visit lifehack_path(review_list.first.lifehack)
-
-    expect_no_page_reload do
-      review_list.each do |review|
-        within(".review-#{review.id}") do
-          click_button '+1'
-        end
-
-        within(".review-#{review.id}") do
-          expect(page).to have_content("Score: 1")
-        end
-      end
-
-      within(".review-#{review_list.first.id}") do
-        expect(page).to have_content("Score: 1")
-      end
-    end
-  end
-
-  scenario 'user can down vote for multiple reviews on the same lifehack', js: true do
-    user_sign_in(user)
-    visit lifehack_path(review_list.first.lifehack)
-
-    expect_no_page_reload do
-      review_list.each do |review|
-        within(".review-#{review.id}") do
-          click_button '-1'
-        end
-
-        within(".review-#{review.id}") do
-          expect(page).to have_content("Score: -1")
-        end
-      end
-
-      within(".review-#{review_list.first.id}") do
-        expect(page).to have_content("Score: -1")
-      end
-    end
-  end
-
-  scenario 'user cannot vote for same review after voting on another review', js: true do
-    user_sign_in(user)
-    visit lifehack_path(review_list.first.lifehack)
-
-    expect_no_page_reload do
-      review_list.each do |review|
-        within(".review-#{review.id}") do
-          click_button '-1'
-        end
-
-        within(".review-#{review.id}") do
-          expect(page).to have_content("Score: -1")
-        end
-      end
-
-      within(".review-#{review_list.first.id}") do
-        click_button '-1'
-
-        expect(page).to have_content("Score: -1")
-      end
-      expect(page).to have_content('You have already downvoted this review!')
-    end
-  end
-
-  scenario 'user can up vote for reviews on different lifehacks', js: true do
-    user_sign_in(user)
-
-    review_different_lifehack.each do |review|
-      visit lifehack_path(review.lifehack)
-
-      expect_no_page_reload do
-        within(".review-#{review.id}") do
-          click_button '+1'
-        end
-
-        within(".review-#{review.id}") do
-          expect(page).to have_content("Score: 1")
-        end
-      end
-    end
+    expect(page).to have_content("Score: -1")
   end
 end
