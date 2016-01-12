cf6e537196b6480a7d8368139fb4a2d5ad026999
jiexu10 <jie.xu.10@gmail.com>
2016-01-12 11:56:39 -0500

    fixed typo in carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 CarrierWave.configure do |config|
   if !Rails.env.test?
-    config.fog_credentials {
+    config.fog_credentials = {
       provider: "AWS",
       aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
       aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
