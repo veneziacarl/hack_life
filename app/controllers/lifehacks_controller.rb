48c76cb885b32cb3de8474817cc26af5997a6bb4
Lawrence Kati <katilaw@gmail.com>
2016-01-13 12:09:37 -0500

    crud actions for lifehack

--- app/controllers/lifehacks_controller.rb
+++ app/controllers/lifehacks_controller.rb
@@ -1,5 +1,7 @@
 class LifehacksController < ApplicationController
   before_action :authenticate_user!, except: [:index]
+  before_action :authorize_user!, only: [:edit, :update, :destroy]
+
   def index
     @lifehacks = Lifehack.order(created_at: :desc)
   end
@@ -41,13 +43,35 @@ class LifehacksController < ApplicationController
 
   def destroy
     @lifehack = Lifehack.destroy(params[:id])
-    redirect_to lifehacks_path,
+    redirect_to root_path,
       notice: "Admin successfully deleted lifehack: #{@lifehack.title}"
+  end
+
+  def edit
+    @lifehack = Lifehack.find(params[:id])
+  end
+
+  def update
+    @lifehack = Lifehack.find(params[:id])
+
+    if @lifehack.update_attributes(lifehack_params)
+      redirect_to @lifehack, notice: 'Lifehack Edited Successfully!'
+    else
+      render :edit, notice: 'You are not the authorized user'
+    end
   end
 
   private
 
   def lifehack_params
     params.require(:lifehack).permit(:title, :description)
+  end
+
+  def authorize_user!
+    user = Lifehack.find(params[:id]).creator
+    unless current_user == user
+      flash[:alert] = "You are not the Authorized User"
+      redirect_to after_sign_in_path_for(current_user)
+    end
   end
 end
---
+++ spec/features/lifehacks/04_user_updates_lifehack_spec.rb
@@ -0,0 +1,68 @@
+require 'rails_helper'
+feature 'user updates lifehack', %{
+  As a User and the creator of the lifehack
+  I want to edit the lifehack
+  So I can update the content of the lifehack
+} do
+
+  # Acceptance Criteria:
+  # [√] I must provide all of the required information
+  # [√] If the information is incorrect, I must be provided errors
+  # [√] If I am not logged in, I cannot modify the content of the lifehack
+  # [√] If I am not the creator, I cannot modify the content of the lifehack
+
+  let (:user) { FactoryGirl.create(:user) }
+  let!(:lifehack) { FactoryGirl.create(:lifehack, creator: user) }
+  let (:user2) { FactoryGirl.create(:user) }
+
+  scenario 'navigate to lifehack show page and edit lifehack' do
+    user_sign_in(user)
+    click_link(lifehack.title)
+    save_and_open_page
+
+    expect(current_path).to eq(lifehack_path(lifehack))
+
+    click_link('Edit Lifehack')
+
+    expect(find_field('lifehack[title]').value).to eq(lifehack.title)
+    expect(find_field('lifehack[description]').value).to eq(lifehack.description)
+
+    title = 'Hopefully this work'
+    description = 'Testing out this cool new edition to the lifehack'
+    fill_in('lifehack[title]', with: title)
+    fill_in('lifehack[description]', with: description)
+    click_button('Add Lifehack')
+
+    expect(current_path).to eq(lifehack_path(lifehack))
+    expect(page).to have_content(title)
+    expect(page).to have_content(description)
+    expect(page).to have_content('Lifehack Edited Successfully!')
+  end
+
+  scenario 'user attempts to post an invalid title' do
+    user_sign_in(user)
+    click_link(lifehack.title)
+    click_link('Edit Lifehack')
+    save_and_open_page
+
+    expect(find_field('lifehack[title]').value).to eq(lifehack.title)
+    expect(find_field('lifehack[description]').value).to eq(lifehack.description)
+
+
+    description = 'How fast can I type this without making a mistake'
+    fill_in('lifehack[title]', with: '')
+    fill_in('lifehack[description]', with: description)
+    click_button('Add Lifehack')
+
+    expect(find_field('lifehack[title]').value).to eq('')
+    expect(find_field('lifehack[description]').value).to eq(description)
+  end
+
+  scenario 'user attempts to edit another user\'s lifehack' do
+    user_sign_in(user2)
+    click_link(lifehack.title)
+    click_link('Edit Lifehack')
+
+    expect(page).to have_content('You are not the Authorized User')
+  end
+end
