2181214a09669edb6e347b89e07e3f620380e305
jiexu10 <jie.xu.10@gmail.com>
2016-01-14 15:34:59 -0500

    fixed bug where webmock was disallowing http connections in development and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,5 @@
 Rails.application.configure do
+  WebMock.allow_net_connect!
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
